08c90ea125f99d9745baece13d7321dae2765b32
Ben Anhalt <anhalt@ku.edu>
2019-10-21 11:13:54 -0500

    prettyprinting.

--- src/Main.hs
+++ src/Main.hs
@@ -4,8 +4,8 @@ import Prelude (fmap, ($), IO)
 
 import Data.Text.IO (putStrLn)
 import SQLSmart (intLit, alias)
-import SQLRender (renderQuery, renderSQL)
-import Upload (parseMappingItem, rowsFromWB, rowsWithValuesFor)
+import SQLRender (renderScript, renderQuery, renderSQL)
+import Upload (findNewRecords, valuesFromWB, parseMappingItem, rowsFromWB, rowsWithValuesFor)
 import ExamplePlan (uploadPlan)
 import UploadPlan (UploadTable(..), mappingItems, uploadTable)
 
@@ -14,6 +14,13 @@ main = do
   let t = alias "t"
   let excludedRows = rowsWithValuesFor $ intLit 0
   let (UploadTable {mappingItems}) = uploadTable uploadPlan
-  let q = rowsFromWB (intLit 0) (fmap (parseMappingItem t) mappingItems) excludedRows
-  let sql = renderSQL (renderQuery q)
-  putStrLn sql
+
+  let rfwb = rowsFromWB (intLit 0) (fmap (parseMappingItem t) mappingItems) excludedRows
+  putStrLn $ renderSQL (renderQuery rfwb)
+
+  let vfwb = valuesFromWB (intLit 0) (fmap (parseMappingItem t) mappingItems) excludedRows
+  putStrLn $ renderSQL (renderQuery vfwb)
+
+  let fnr = findNewRecords (intLit 0) (uploadTable uploadPlan)
+  putStrLn $ renderSQL (renderScript fnr)
+  
--- src/SQLRender.hs
+++ src/SQLRender.hs
@@ -1,300 +1,144 @@
 module SQLRender where
 
-import qualified Prelude as P
-import Prelude (Maybe(..), id, (.), ($), pure, fmap, mempty, sequence_, (<>), (>>=))
-import Data.Text (pack, Text)
-import Data.List (intercalate)
-import Control.Monad (forM_)
-import Control.Monad.Writer (execWriter, Writer, tell)
+import Prelude (Maybe(..), id, (.), ($), fmap, mempty, (<>))
+import Data.Text (Text)
+import Data.Text.Prettyprint.Doc (nest, indent, group, line, sep, (<+>), vsep, comma, hsep, punctuate, parens, squotes, pretty, defaultLayoutOptions, layoutPretty, Doc)
+import Data.Text.Prettyprint.Doc.Render.Text (renderStrict)
 
 import SQL (OrderTerm(..), JoinSpec(..), ColumnName(..), TableName(..), JoinedTable(..), TableFactor(..), TableRef(..), SelectTerm(..), SelectType(..), QueryExpr(..), SubQuery(..), FCall(..), Literal(..), BinOp(..), Expr(..), CompOp(..), Alias(..), Identifier(..), Statement(..), Script(..))
 
-type SQL = Writer Text ()
 
-show :: forall a. P.Show a => a -> Text
-show = pack . P.show
+notImplemented :: forall a b. a -> Doc b
+notImplemented _ =  "<not implemented>"
 
-notImplemented :: forall a. a -> SQL
-notImplemented _ = do
-  tell "<not implemented>"
+renderSQL :: Doc a -> Text
+renderSQL = renderStrict . layoutPretty defaultLayoutOptions
 
-renderSQL :: SQL -> Text
-renderSQL s = execWriter s
+renderScript :: Script -> Doc a
+renderScript (Script statements) = vsep $ fmap renderStatement statements
 
-separatedBy :: forall a. SQL -> (a -> SQL) -> [a] -> SQL
-separatedBy separator render xs =
-  sequence_ $ intercalate [separator] $ fmap (pure . render) xs
+renderStatement :: Statement -> Doc a
+renderStatement (QueryStatement q) = renderQuery q <> ";"
+renderStatement (SetUserVar n v) = "set" <+> "@" <> (pretty $ escapeIdentifier n) <+> "=" <+> renderExpr v <> ";"
+renderStatement (InsertValues {tableName, columns, values}) =
+  group $ vsep
+  [ "insert into"
+  , (group $ renderTableName tableName <> line <> (parens $ sep $ punctuate comma $ fmap renderColumnName columns))
+  , "values"
+  , (vsep $ punctuate comma $ fmap (parens . hsep . punctuate comma . fmap renderExpr) values)
+  ] <> ";"
 
-commaSeparated :: forall a. (a -> SQL) -> [a] -> SQL
-commaSeparated = separatedBy cs
+renderStatement (InsertFrom {tableName, columns, queryExpr}) =
+  group $ vsep
+  [ "insert into"
+  , (group $ renderTableName tableName <> line <> (parens $ sep $ punctuate comma $ fmap renderColumnName columns))
+  , renderQuery queryExpr
+  ] <> ";"
 
-nl :: SQL
-nl = tell "\n"
 
-sp :: SQL
-sp = tell " "
+renderAlias :: Alias -> Doc a
+renderAlias (Alias a) = pretty $ escapeIdentifier a
 
-cs :: SQL
-cs = tell ", "
-
-cnl :: SQL
-cnl = tell ",\n"
-
-endStatement :: SQL
-endStatement = tell ";\n"
-
-inParens :: SQL -> SQL
-inParens s = do
-  tell "("
-  s
-  tell ")"
-
-renderScript :: Script -> SQL
-renderScript (Script statements) = forM_ statements renderStatement
-
-renderStatement :: Statement -> SQL
-renderStatement (QueryStatement q) = do
-  renderQuery q
-  endStatement
-
-renderStatement (InsertValues {tableName, columns, values}) = do
-  tell "insert into"
-  sp
-  renderTableName tableName
-  sp
-  inParens $ commaSeparated renderColumnName columns
-  nl
-  tell "values"
-  nl
-  separatedBy cnl (inParens . commaSeparated renderExpr) values
-  endStatement
-
-renderStatement (InsertFrom {tableName, columns, queryExpr}) = do
-  tell "insert into"
-  sp
-  renderTableName tableName
-  sp
-  inParens $ commaSeparated renderColumnName columns
-  nl
-  renderQuery queryExpr
-  endStatement
-
-renderStatement (SetUserVar n v) = do
-  tell "set "
-  tell "@"
-  tell $ escapeIdentifier n
-  tell " = "
-  renderExpr v
-  endStatement
-
-renderAlias :: Alias -> SQL
-renderAlias (Alias a) = tell $ escapeIdentifier a
-
-renderQuery :: QueryExpr -> SQL
+renderQuery :: QueryExpr -> Doc a
 renderQuery (QueryExpr {selectType, selectTerms, fromExpr, where_, ordering}) = case selectTerms of
-  [] -> tell mempty
-  _ -> do
-    renderSelectType selectType
-    sp
-    commaSeparated renderSelectTerm selectTerms
-    renderFrom fromExpr
-    renderWhere where_
-    renderOrderBy ordering
+  [] -> mempty
+  _ -> group (
+    group $ renderSelectType selectType
+      <> (nest 4 $ line <> (sep $ punctuate comma $ fmap renderSelectTerm selectTerms))
+    )
+    <> renderFrom fromExpr
+    <> renderWhere where_
+    <> renderOrderBy ordering
   -- renderLimit q.limit
 
-renderWhere :: Maybe Expr -> SQL
-renderWhere Nothing = tell mempty
-renderWhere (Just x) = do
-  nl
-  tell "where "
-  renderExpr x
+renderWhere :: Maybe Expr -> Doc a
+renderWhere Nothing = mempty
+renderWhere (Just x) = line <> "where" <+> renderExpr x
 
-renderOrderBy :: [OrderTerm] -> SQL
-renderOrderBy [] = tell mempty
-renderOrderBy terms = do
-  nl
-  tell "order by"
-  sp
-  commaSeparated renderOrderTerm terms
+renderOrderBy :: [OrderTerm] -> Doc a
+renderOrderBy [] = mempty
+renderOrderBy terms = line <> "order by" <+> (hsep $ punctuate comma $ fmap renderOrderTerm terms)
   where
-    renderOrderTerm (Ascending x) = renderExpr x >>= \_ -> tell " asc"
-    renderOrderTerm (Descending x) = renderExpr x >>= \_ -> tell " desc"
+    renderOrderTerm (Ascending x) = renderExpr x <+> "asc"
+    renderOrderTerm (Descending x) = renderExpr x <+> "desc"
 
-renderSelectType :: SelectType -> SQL
-renderSelectType SelectAll = tell "select"
-renderSelectType SelectDistinct = tell "select distinct"
-renderSelectType SelectDistinctRow = tell "select distinctrow"
+renderSelectType :: SelectType -> Doc a
+renderSelectType SelectAll = "select"
+renderSelectType SelectDistinct = "select distinct"
+renderSelectType SelectDistinctRow = "select distinctrow"
 
-renderSelectTerm :: SelectTerm -> SQL
-renderSelectTerm Star = tell "*"
-renderSelectTerm (StarFrom (Alias c)) = tell $ c <> ".*"
+renderSelectTerm :: SelectTerm -> Doc a
+renderSelectTerm Star = "*"
+renderSelectTerm (StarFrom a) = renderAlias a <> ".*"
 renderSelectTerm (SelectTerm expr) = renderExpr expr
-renderSelectTerm (SelectAs as expr) = do
-  renderExpr expr
-  sp
-  tell "as"
-  sp
-  tell as
+renderSelectTerm (SelectAs as expr) = renderExpr expr <+> "as" <+> (pretty $ escapeIdentifier as)
 
-renderFrom :: [TableRef] -> SQL
+
+renderFrom :: [TableRef] -> Doc a
 renderFrom trs = case trs of
-  [] -> tell mempty
-  _ -> do
-    nl
-    tell "from "
-    commaSeparated renderTableRef trs
+  [] -> mempty
+  _ -> line <> "from" <+> (sep $ punctuate comma $ fmap renderTableRef trs)
 
-renderTableRef :: TableRef -> SQL
+renderTableRef :: TableRef -> Doc a
 renderTableRef (TableFactor tf) = renderTableFactor tf
 renderTableRef (JoinedTable jt) = renderJoinedTable jt
 
-renderJoinedTable :: JoinedTable -> SQL
-renderJoinedTable (InnerJoin l r o) = do
-  renderTableRef l
-  tell " join "
-  renderTableRef r
+renderJoinedTable :: JoinedTable -> Doc a
+renderJoinedTable (InnerJoin l r o) = group $ renderTableRef l <> line <> "join" <+> renderTableRef r <+>
   case o of
-    Nothing -> tell mempty
+    Nothing -> mempty
     Just on -> renderJoinSpec on
 
-renderJoinedTable (LeftJoin l r o) = do
-  renderTableRef l
-  tell " left join "
-  renderTableRef r
-  renderJoinSpec o
-
+renderJoinedTable (LeftJoin l r o) = group $ renderTableRef l <> line <> "left join" <+> renderTableRef r <+> renderJoinSpec o
 renderJoinedTable p@(RightJoin _ _ _) = notImplemented p
 renderJoinedTable p@(NaturalLeftJoin _ _) = notImplemented p
 renderJoinedTable p@(NaturalRightJoin _ _) = notImplemented p
 
 
-renderJoinSpec :: JoinSpec -> SQL
-renderJoinSpec (JoinOn expr) = do
-  tell " on "
-  renderExpr expr
+renderJoinSpec :: JoinSpec -> Doc a
+renderJoinSpec (JoinOn expr) = line <> (indent 2 $ "on" <+> renderExpr expr)
 
 renderJoinSpec (JoinUsing cols) = case cols of
-  [] -> tell mempty
-  _  -> do
-    tell " using "
-    inParens $ commaSeparated renderColumnName cols
+  [] -> mempty
+  _  -> "using" <+> (parens $ hsep $ punctuate comma $ fmap renderColumnName cols)
 
-renderColumnName :: ColumnName -> SQL
-renderColumnName (ColumnName n) = tell $ escapeIdentifier n
+renderColumnName :: ColumnName -> Doc a
+renderColumnName (ColumnName n) = pretty $ escapeIdentifier n
 
-renderTableFactor :: TableFactor -> SQL
+renderTableFactor :: TableFactor -> Doc a
 renderTableFactor p@(Tables _) = notImplemented p
-renderTableFactor Dual = tell "dual"
-renderTableFactor (Table {name, as}) = do
-  renderTableName name
+renderTableFactor Dual = "dual"
+renderTableFactor (Table {name, as}) = renderTableName name <>
   case as of
-    Just (Alias a) -> do
-      tell " as "
-      tell a
-    Nothing ->
-      tell mempty
+    Just a -> " as" <+> renderAlias a
+    Nothing -> mempty
 
-renderTableFactor (Query (SubQuery {subquery, alias})) = do
-  inParens $ do
-    nl
-    renderQuery subquery
-    nl
-  tell " as "
-  renderAlias alias
+renderTableFactor (Query (SubQuery {subquery, alias})) = group $ renderSubQuery subquery <+> "as" <+> renderAlias alias
 
-renderTableName :: TableName -> SQL
-renderTableName (TableName n) = tell $ escapeIdentifier n
 
-renderExpr :: Expr -> SQL
-renderExpr (OrExpr x y) = inParens $ do
-  renderExpr x
-  sp
-  tell "or"
-  sp
-  renderExpr y
+renderSubQuery :: QueryExpr -> Doc a
+renderSubQuery sq = parens $ nest 4 $ line <> renderQuery sq <> line
 
-renderExpr (XorExpr x y) = inParens $ do
-  renderExpr x
-  sp
-  tell "xor"
-  sp
-  renderExpr y
 
-renderExpr (AndExpr x y) = inParens $ do
-  renderExpr x
-  sp
-  tell "and"
-  sp
-  renderExpr y
+renderTableName :: TableName -> Doc a
+renderTableName (TableName n) = pretty $ escapeIdentifier n
 
-renderExpr (NotExpr x) = inParens $ do
-  tell "not"
-  sp
-  renderExpr x
-
-renderExpr (IsTrueExpr p) = inParens $ do
-  renderExpr p
-  sp
-  tell "is true"
-
-renderExpr (IsFalseExpr p) = inParens $ do
-  renderExpr p
-  sp
-  tell "is false"
-
-renderExpr (IsUnknownExpr p) = inParens $ do
-  renderExpr p
-  sp
-  tell "is unknown"
-
-renderExpr (IsNull p) = inParens $ do
-  renderExpr p
-  sp
-  tell "is null"
-
-renderExpr (IsNotNull p) = inParens $ do
-  renderExpr p
-  sp
-  tell "is not null"
-
-renderExpr (IsNotDistinctFrom x y) = inParens $ do
-  renderExpr x
-  sp
-  tell "<=>"
-  sp
-  renderExpr y
-
-renderExpr (CompOp op x y) = inParens $ do
-  renderExpr x
-  sp
-  renderCompOp op
-  sp
-  renderExpr y
-
+renderExpr :: Expr -> Doc a
+renderExpr (OrExpr x y) = parens $ renderExpr x <+> "or" <+> renderExpr y
+renderExpr (XorExpr x y) = parens $ renderExpr x <+> "xor" <+> renderExpr y
+renderExpr (AndExpr x y) = parens $ renderExpr x <+> "and" <+> renderExpr y
+renderExpr (NotExpr x) = parens $ "not" <+> renderExpr x
+renderExpr (IsTrueExpr p) = parens $ renderExpr p <+> "is true"
+renderExpr (IsFalseExpr p) = parens $ renderExpr p <+> "is false"
+renderExpr (IsUnknownExpr p) = parens $ renderExpr p <+> "is unknown"
+renderExpr (IsNull p) = parens $ renderExpr p <+> "is null"
+renderExpr (IsNotNull p) = parens $ renderExpr p <+> "is not null"
+renderExpr (IsNotDistinctFrom x y) = parens $ renderExpr x <+> "<=>" <+> renderExpr y
+renderExpr (CompOp op x y) = parens $ renderExpr x <+> renderCompOp op <+> renderExpr y
 renderExpr p@(CompAll _ _ _) = notImplemented p
 renderExpr p@(CompAny _ _ _) = notImplemented p
-
-renderExpr (InPred x sq) = inParens $ do
-  renderExpr x
-  sp
-  tell "in"
-  sp
-  inParens $ do
-    nl
-    renderQuery sq
-    nl
-
-renderExpr (NotInPred x sq) = inParens $ do
-  renderExpr x
-  sp
-  tell "not in"
-  sp
-  inParens $ do
-    nl
-    renderQuery sq
-    nl
-
-
+renderExpr (InPred x sq) = parens $ renderExpr x <+> "in" <+> renderSubQuery sq
+renderExpr (NotInPred x sq) = parens $ renderExpr x <+> "not in" <+> renderSubQuery sq
 renderExpr p@(Between _ _ _) = notImplemented p
 renderExpr p@(NotBetween _ _ _) = notImplemented p
 renderExpr p@(SoundsLike _ _) = notImplemented p
@@ -302,74 +146,40 @@ renderExpr p@(Like _ _) = notImplemented p
 renderExpr p@(NotLike _ _) = notImplemented p
 renderExpr p@(Regexp _ _) = notImplemented p
 renderExpr p@(NotRegexp _ _) = notImplemented p
-
-
-renderExpr (BinOp op x y) = inParens $ do
-  renderExpr x
-  sp
-  renderBinOp op
-  sp
-  renderExpr y
-
-
+renderExpr (BinOp op x y) = parens $ renderExpr x <+> renderBinOp op <+> renderExpr y
 renderExpr (Literal l) = renderLiteral l
 renderExpr (IdentExpr i) = renderIdentifier i
-
-renderExpr (FCallExpr (FCall {fname, args})) = do
-  tell fname
-  inParens $ commaSeparated renderExpr args
-
-renderExpr (VarExpr s) = tell $ ("@" <> escapeIdentifier s)
-
+renderExpr (FCallExpr (FCall {fname, args})) = pretty fname <> (parens $ hsep $ punctuate comma $ fmap renderExpr args)
+renderExpr (VarExpr s) = pretty $ ("@" <> escapeIdentifier s)
 renderExpr p@(UnaryOp _ _) = notImplemented p
-
-renderExpr (RowExpr values) = inParens $ commaSeparated renderExpr values
-
-renderExpr (SubQueryExpr (SubQuery {subquery, alias})) = do
-  inParens $ do
-    nl
-    renderQuery subquery
-    nl
-  tell " as "
-  renderAlias alias
-
-
+renderExpr (RowExpr values) = parens $ hsep $ punctuate comma $ fmap renderExpr values
+renderExpr (SubQueryExpr (SubQuery {subquery, alias})) = renderSubQuery subquery <+> "as" <+> renderAlias alias
 renderExpr p@(Exists _) = notImplemented p
-renderExpr (RawExpr s) = inParens $ tell s
-renderExpr (Assignment s x) = inParens $ do
-  tell s
-  tell " := "
-  renderExpr x
+renderExpr (RawExpr s) = parens $ pretty s
+renderExpr (Assignment s x) = parens $ pretty s <+> ":=" <+> renderExpr x
 
-renderBinOp :: BinOp -> SQL
-renderBinOp AndOp = tell "&"
-renderBinOp OrOp = tell "|"
-renderBinOp PlusOp = tell "+"
-renderBinOp MinusOp = tell "-"
+renderBinOp :: BinOp -> Doc a
+renderBinOp AndOp =  "&"
+renderBinOp OrOp =  "|"
+renderBinOp PlusOp =  "+"
+renderBinOp MinusOp =  "-"
 
-renderCompOp :: CompOp -> SQL
-renderCompOp E_Op = tell "="
-renderCompOp GE_Op = tell ">="
-renderCompOp G_Op = tell ">"
-renderCompOp LE_Op = tell "<="
-renderCompOp L_Op = tell "<"
-renderCompOp NE_Op = tell "<>"
+renderCompOp :: CompOp -> Doc a
+renderCompOp E_Op =  "="
+renderCompOp GE_Op =  ">="
+renderCompOp G_Op =  ">"
+renderCompOp LE_Op =  "<="
+renderCompOp L_Op =  "<"
+renderCompOp NE_Op =  "<>"
 
-renderLiteral :: Literal -> SQL
-renderLiteral (IntLit i) = tell $ show i
-renderLiteral (FloatLit n) = tell $ show n
-renderLiteral (TextLit s) = do
-  tell "'"
-  tell $ escapeText s
-  tell "'"
+renderLiteral :: Literal -> Doc a
+renderLiteral (IntLit i) = pretty i
+renderLiteral (FloatLit n) = pretty n
+renderLiteral (TextLit s) = squotes $ pretty $ escapeText s
 
-renderIdentifier :: Identifier -> SQL
-renderIdentifier (Identifier s) = tell $ escapeIdentifier s
-renderIdentifier (Qualified (Alias a) s) = do
-  tell $ escapeIdentifier a
-  tell "."
-  tell $ escapeIdentifier s
-
+renderIdentifier :: Identifier -> Doc a
+renderIdentifier (Identifier s) = pretty $ escapeIdentifier s
+renderIdentifier (Qualified (Alias a) s) = (pretty $ escapeIdentifier a) <> "." <> (pretty $ escapeIdentifier s)
 
 escapeText :: Text -> Text
 escapeText = id -- todo
