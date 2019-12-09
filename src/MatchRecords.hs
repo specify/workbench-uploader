8a40bd5dff2b6900f2d98ff67ea908916b794d38
Ben Anhalt <anhalt@ku.edu>
2019-12-09 14:26:14 -0600

    inserts!

--- src/MatchRecords.hs
+++ src/MatchRecords.hs
@@ -1,18 +1,21 @@
 module MatchRecords where
 
 import Data.Text (Text)
-import Prelude (fst, (.), const, Int, head, foldl1, (<$>), Maybe(..), pure, ($), (==), undefined, (<>), fmap)
+import Prelude ((<$), fst, (.), const, Int, head, foldl1, (<$>), Maybe(..), pure, ($), (==), undefined, (<>), fmap, maybe)
 import Control.Monad (forM_)
+import Control.Newtype.Generics (unpack)
 import Data.Maybe (isJust, catMaybes, fromMaybe)
 import Data.List.Index (ifoldl, imap)
 import qualified Data.List as L
 import qualified Data.List.Extra as L
 import Data.Tuple.Extra (fst3)
+import Database.MySQL.Simple.Types (Only(..))
+
 import UploadPlan (NamedValue(..), ColumnType, WorkbenchId(..), UploadPlan(..), MappingItem(..), UploadStrategy(..), ToManyRecord(..), ToMany(..), ToOne(..), UploadTable(..))
-import SQL (ColumnName(..), Alias, SelectTerm, TableRef, Expr, QueryExpr)
+import SQL (Statement, ColumnName(..), Alias, SelectTerm, TableRef, Expr, QueryExpr)
 import MonadSQL (MonadSQL(..))
 import SQLSmart
- (rollback, union, createTempTable, startTransaction,  (<=>)
+ (insertFrom, rollback, union, createTempTable, startTransaction,  (<=>)
  , (@@)
  , alias
  , and
@@ -47,7 +50,7 @@ import SQLSmart
  , using
  )
 import MatchExistingRecords (flagNewRecords, useFirst, findExistingRecords)
-import Common (mappingId, parseValue, selectFromWBas, parseMappingItem)
+import Common (tableColumn, mappingId, parseValue, selectFromWBas, parseMappingItem)
 import qualified Common
 
 
@@ -60,9 +63,9 @@ data ColumnDescriptor = ColumnDescriptor
 data ColumnSource = WBValue (Int, ColumnType) | StaticValue Expr
 
 
-columnDescriptorsForTable :: [ColumnName] -> UploadTable -> [ColumnDescriptor]
+columnDescriptorsForTable :: [Text] -> UploadTable -> [ColumnDescriptor]
 columnDescriptorsForTable columns (UploadTable {mappingItems, staticValues}) = do
-  (i, ColumnName c) <- L.zip [0 ..] columns
+  (i, c) <- L.zip [0 ..] columns
   case L.find ((==) c . columnName) mappingItems of
     Just (MappingItem {id, columnType}) ->
       pure $ ColumnDescriptor {colName = c, colSource = WBValue (id, columnType), position = i}
@@ -73,8 +76,8 @@ columnDescriptorsForTable columns (UploadTable {mappingItems, staticValues}) = d
         Nothing ->
           pure $ ColumnDescriptor {colName = c, colSource = StaticValue null, position = i}
 
-columnsForTableGroup :: [UploadTable] -> [ColumnName]
-columnsForTableGroup uploadTables = ColumnName <$> (L.nub $ L.sort $ (staticColumns <> mappingColumns))
+columnsForTableGroup :: [UploadTable] -> [Text]
+columnsForTableGroup uploadTables = L.nub $ L.sort (staticColumns <> mappingColumns)
   where
     staticColumns = do
       UploadTable {staticValues} <- uploadTables
@@ -134,18 +137,34 @@ uploadLeafRecords up@(UploadPlan {uploadTable, workbenchId}) = do
   execute $ startTransaction
   let groups = uploadGroups uploadTable
   forM_ groups $ \group -> do
+    let UploadTable {tableName} = head group
     let columns = columnsForTableGroup group
     let queries = (\ut -> valuesFromWB workbenchId ut (columnDescriptorsForTable columns ut)) <$> group
-    execute $ createTempTable ("newvalues" <> tableName (head group)) $ foldl1 union queries
+    execute $ createTempTable ("newvalues_" <> tableName) $ foldl1 union queries
+    execute $ insertNewRecords tableName columns
+    -- lastInsert <- doQuery (query [select $ rawExpr "last_insert_id()"])
+    -- let lastInsertId = case lastInsert of
+    --       [Only id] -> id :: Int
+    -- _a
   execute $ rollback
+
+insertNewRecords :: Text -> [Text] -> Statement
+insertNewRecords tableName columns = insertFrom tableName (columns <> extraColumns) $
+      query ((select . project <$> columns) <> (select <$> extraValues))
+      `from` [table $ "newvalues_" <> tableName]
+      `orderBy` (asc . project <$> columns)
+  where
+    extraColumns = ["version", "timestampcreated", "guid"]
+    extraValues = rawExpr <$> ["0", "now()", "uuid()"]
+
 
 valuesFromWB :: WorkbenchId -> UploadTable -> [ColumnDescriptor] -> QueryExpr
 valuesFromWB (WorkbenchId wbId) (UploadTable {idMapping}) descriptors =
-  newValuesFromWB (intLit wbId) (nullable intLit idMapping) descriptors
+  newValuesFromWB (intLit wbId) (maybe null intLit idMapping) descriptors
 
 newValuesFromWB :: Expr -> Expr -> [ColumnDescriptor] -> QueryExpr
 newValuesFromWB wbId idMappingId descriptors =
-  queryDistinct (selectWBVal <$> descriptors)
+  queryDistinct ((selectAs "idForUpload" null) : (selectWBVal <$> descriptors))
   `from`
   [ L.foldl joinWBCell (table "workbenchrow" `as` r) wbCols
     `join` (table "workbenchdataitem" `as` idCol)
@@ -157,7 +176,7 @@ newValuesFromWB wbId idMappingId descriptors =
   `suchThat`
   (((r @@ "workbenchid") `equal` wbId) `and` (r @@ "uploadstatus" `equal` intLit 0))
   `having`
-  (not $ (row $ (project . fst3) <$> wbCols) <=> (row $ (const null) <$> wbCols))
+  (not $ (row $ project . fst3 <$> wbCols) <=> (row $ null <$ wbCols))
   where
     idCol = alias "idcol"
     r = alias "r"
@@ -183,6 +202,32 @@ newValuesFromWB wbId idMappingId descriptors =
         `and` (((c i) @@ "workbenchtemplatemappingitemid") `equal` (intLit mappingId))
       )
 
-nullable :: (a -> Expr) -> Maybe a -> Expr
-nullable toExpr v = fromMaybe null $ toExpr <$> v
 
+-- findNewRecords :: Expr -> UploadTable -> [Statement]
+-- findNewRecords wbTemplateMappingItemId ut@(UploadTable {idColumn, mappingItems}) =
+--   [ setUserVar "new_id" $ rawExpr "last_insert_id()"
+--   , setUserVar "row_number" $ intLit 0
+--   , UpdateStatement $ update
+--     [ (table "workbenchdataitem")
+--       `join`
+--       (subqueryAs wbRow $ rowsFromWB (userVar "workbenchid") mappingItems' excludeRows) `using` ["workbenchrowid"]
+--       `join`
+--       (subqueryAs valuesWithId $
+--        query
+--        [ selectAs idColumn $ (userVar "row_number") `plus` (userVar "new_id")
+--        , select $ "row_number" @= ((userVar "row_number") `plus` (intLit 1))
+--        , starFrom newValues
+--        ] `from`
+--        [ subqueryAs newValues $ valuesFromWB (userVar "workbenchid") mappingItems' excludeRows ]
+--       ) `on` ( newVals <=> wbVals )
+--     ]
+--     [("celldata", valuesWithId @@ idColumn)]
+--   ]
+--   where
+--     t = alias "t"
+--     wbRow = alias "wbrow"
+--     newValues = alias "newvalues"
+--     valuesWithId = alias "valueswithid"
+--     mappingItems' = fmap (parseMappingItem t) mappingItems <> toOneMappingItems ut t <> toManyMappingItems ut
+--     newVals = row $ fmap (\(MappingItem {selectFromWBas}) -> valuesWithId @@ selectFromWBas) mappingItems'
+--     wbVals = row $ fmap (\(MappingItem {selectFromWBas}) -> wbRow @@ selectFromWBas) mappingItems'
